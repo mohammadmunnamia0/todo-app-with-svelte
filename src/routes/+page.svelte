0c7e6d114fed16aba1ea2e03aab504a29ec7dd3f
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-06-14 16:22:07 +0600

    Initialized the project with the required packages

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+import '@picocss/pico/css/pico.min.css' 
+import Form from './form.svelte';
+ let message = "ToDo App";
+</script>
+
+<main>
+<h1>{message}</h1>
+ <Form/>
+</main>
+
+<style>
+main{
+    margin : 1rem auto;
+    max-width: 800px;
+}
+</style>
---
+++ src/routes/form.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    let newTask = $state('');
+
+    function formSubmitted(e: SubmitEvent){
+        e.preventDefault();
+        console.log(newTask)
+    }
+
+</script>
+
+<form onsubmit={formSubmitted}>
+    <label for="">Task
+        <input type="newTask" bind:value={newTask}>
+    </label>
+    <button>Add</button>
+</form>
